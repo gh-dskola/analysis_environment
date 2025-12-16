d0d5cbe36dbe628091f311d8ee6c4183f1269b01
Dylan Skola <dskola@devana10.hpc.gh>
2025-12-16 15:44:22 -0800

    Initial files for launching jupyter lab docker on first available port

---
+++ jupyter_docker/launch_jupyter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo Inside launch script
+echo $HOST_NAME
+echo $HOST_PORT
+mamba run -n jupyter -c jupyter lab \
+    --ServerApp.custom_display_url "http://$HOST_NAME.ghdna.io:$HOST_PORT" \
+    --ServerApp.default_url "http://$HOST_NAME.ghdna.io:$HOST_PORT" \
+    --ip 0.0.0.0 \
+    --port 8888 \
+    --no-browser \
+    --allow-root
