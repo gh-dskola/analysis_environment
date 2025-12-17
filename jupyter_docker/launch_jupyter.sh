9ec96e4f94c754075793ed2d53abae12c701c9d6
Dylan Skola <dskola@guardanthealth.com>
2025-12-16 16:03:56 -0800

    Added docker build command, bind mounts

--- jupyter_docker/launch_jupyter.sh
+++ jupyter_docker/launch_jupyter.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo Inside launch script
-echo $HOST_NAME
-echo $HOST_PORT
+# echo Inside launch script
+# echo $HOST_NAME
+# echo $HOST_PORT
+cd /workspace
 mamba run -n jupyter -c jupyter lab \
     --ServerApp.custom_display_url "http://$HOST_NAME.ghdna.io:$HOST_PORT" \
     --ServerApp.default_url "http://$HOST_NAME.ghdna.io:$HOST_PORT" \
